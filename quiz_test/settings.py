ab900bd23132e4c637de7c6f5cd33fdb6807afe5
mbicl <baxriddinovmaqsud@gmail.com>
2023-06-17 21:59:29 +0500

    added allowed host and trusted csrf origin

--- quiz_test/settings.py
+++ quiz_test/settings.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from decouple import config
 from os import path
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -14,9 +14,9 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['quiztest-production.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = ['https://quiztest-production.up.railway.app','mbicl.jprq.io']
+CSRF_TRUSTED_ORIGINS = ['https://quiztest-production.up.railway.app']
 
 # Application definition
 
@@ -64,7 +64,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "quiz_test.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -74,7 +73,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -94,7 +92,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -105,7 +102,6 @@ TIME_ZONE = "Asia/Tashkent"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
