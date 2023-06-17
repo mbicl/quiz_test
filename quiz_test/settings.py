7e2defc16cf63ab3185d6378a02977a0140c91bc
mbicl <baxriddinovmaqsud@gmail.com>
2023-06-17 21:54:10 +0500

    added trusted origins and railway.json file for deploying

--- quiz_test/settings.py
+++ quiz_test/settings.py
@@ -16,7 +16,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://quiztest-production.up.railway.app','mbicl.jprq.io']
 
 # Application definition
 
