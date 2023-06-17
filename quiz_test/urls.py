813678735153ab3e0efa4395ed20dbe7fd38ea1b
mbicl <baxriddinovmaqsud@gmail.com>
2023-06-17 22:54:04 +0500

    debug=false and admin url

--- quiz_test/urls.py
+++ quiz_test/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/mbi/admin/", admin.site.urls),
     path("accounts/", include("users.urls")),
     path("", include("test_app.urls")),
     path("ckeditor/", include("ckeditor_uploader.urls")),
